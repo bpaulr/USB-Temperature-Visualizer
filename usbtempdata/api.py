98776a42be5e534238005eb5d5727f4fe0847c06
bradleypaulread <bradleypaulread@gmail.com>
2020-05-28 10:52:12 +0100

    Added get graph function to api

--- usbtempdata/api.py
+++ usbtempdata/api.py
@@ -8,5 +8,13 @@ def hello():
     return "pong"
 
 
+@app.route("/")
+def get_graph():
+    contents = "null"
+    with open("data/graph.html", "r") as graph:
+        contents = graph.read()
+    return contents
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
